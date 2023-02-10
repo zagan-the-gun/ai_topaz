6bff0a824bfc1ad5dbd1be40c88df9bb611c8efc
zagan-the-gun <zagan.the.gun@gmail.com>
2023-02-10 20:37:48 +0900

    コードリファクタリング

--- ai_topaz/tasks.py
+++ ai_topaz/tasks.py
@@ -1,6 +1,5 @@
 from __future__ import absolute_import, unicode_literals
 
-#from .celery import app
 from celery import shared_task
 from api.schemas import ArtIn
 from api.models import Art
@@ -10,25 +9,15 @@ import subprocess
 import pprint
 
 
-print('DEAD BEEF tasks 1')
-#print(app.conf.broker_url)
-
-
-#@app.task
 @shared_task
 def add(x, y):
     return x + y
 
-import time
 
 @shared_task
 def txt2img_task(art_id):
     print('txt2img_task開始')
 
-    #subprocess.run(f'/home/ishizuka/txt2img_v2.sh {filename} "{t2i.prompt}" {t2i.seed} {t2i.n_iter} {t2i.scale} {t2i.ddim_steps}', shell=True)
-    #img = {'file': open(f"/home/ishizuka/stable-diffusion/outputs/txt2img-samples/{filename}-grid.png", 'rb')}
-
-    time.sleep(1)
     print(f'txt2img_task依頼art_id: {art_id}')
     try:
         art = Art.objects.get(pk=art_id)
@@ -39,25 +28,8 @@ def txt2img_task(art_id):
     # バリデーションしっかりしないとヤバい
     subprocess.run(f'/home/ishizuka/dev-txt2img.sh {art.file_name} "{art.prompt}" {art.seed} {art.n_iter} {art.scale} {art.ddim_steps}', shell=True)
 
-    # ファイル保存場所はS3にしよう
-    #img = {'file': open(f"/home/ishizuka/stable-diffusion/outputs/txt2img-samples/{art.file_name}-grid.png", 'rb')}
-
-    #subprocess.run(f"rm /home/ishizuka/stable-diffusion/outputs/txt2img-samples/{art.file_name}-grid.png", shell=True)
-    #subprocess.run(f"rm /home/ishizuka/stable-diffusion/outputs/txt2img-samples/samples/{art.file_name}*.png", shell=True)
-
     art.is_generated = True
     art.save()
 
     print('txt2img_task終了')
-
-    #return f'DEAD BEEF OK!'
-
-
-#@app.task
-#def mul(x, y):
-#    return x * y
-
-#@app.task
-#def xsum(numbers):
-#    return sum(numbers)
 
--- api/api.py
+++ api/api.py
@@ -1,7 +1,6 @@
 from ninja import NinjaAPI
 from pydantic import BaseModel
 from typing import Union
-#from .tasks import add
 from ai_topaz.tasks import add, txt2img_task
 from .schemas import ArtIn
 from .models import Art
@@ -21,47 +20,17 @@ class T2I(BaseModel):
 
 @api.post("/txt2img")
 def txt2img(request, art: ArtIn):
-#def txt2img(request, t2i: T2I):
-    #res = add.delay(x, y)
-    #res = txt2img_task(art, 'DEAD_BEEF')
-    #print(f'{res}')
-    #txt2img_task(art, 'DEAD_BEEF')
-
     user = User.objects.get(pk=1)
-    #art = Art.objects.create(user=user, prompt='', seed='', scale='', ddim_steps='', n_iter='')
-    print('DEAD BEEF n_iter')
-    print(art.n_iter)
-    print(type(art.n_iter))
-    #art = Art.objects.create(user=user, prompt=art.prompt, seed=art.seed, n_iter=art.n_iter, scale=art.scale, ddim_steps=art.ddim_steps, )
-    art = Art.objects.create(user=user, prompt=art.prompt, seed=39, )
+    art = Art.objects.create(user=user, prompt=art.prompt, seed=art.seed, n_iter=art.n_iter, scale=art.scale, ddim_steps=art.ddim_steps, )
 
     txt2img_task.delay(art.id)
-    # 再起動面倒なので開発中は直接動かす
+    # celeryの再起動面倒なので開発中は直接動かす
     #txt2img_task(art.id)
 
     print(f'{art.file_name}')
     return JsonResponse({"filename": art.file_name})
 
-@api.get("/img/{file_name}")
-async def get_img(request, file_name):
-    print(file_name)
-    is_file = os.path.isfile(f"/home/ishizuka/stable-diffusion/outputs/txt2img-samples/{file_name}.png")
-    if is_file:
-        return FileResponse(f"/home/ishizuka/stable-diffusion/outputs/txt2img-samples/{file_name}.png")
-    else:
-        response.status_code = status.HTTP_202_ACCEPTED
-        return
-
 @api.get("/get_test")
 def get_test(request):
     return "get test!"
-
-@api.get("/hello")
-def hello(request, x:int=1 , y:int=1):
-    #res = add(x, y)
-    res = add.delay(x, y)
-    #res = add.apply_async(x, y)
-    #res = add.apply_async((x, y), queue='lopri', countdown=10)
-
-    return f"hello {x, y, res}"
 
